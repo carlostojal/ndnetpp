58cb76d49800a1ddc8012a5c0b2d830d7ed2d233
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-08-16 18:57:16 +0100

    remove the old kernel code

--- nd_utils/voxelize.cpp
+++
@@ -1,9 +0,0 @@
-#include "voxelize.h"
-
-std::vector<torch::Tensor> voxelize_forward(
-    torch::Tensor points,
-    torch::Tensor desired_n_voxels) {
-
-        // TODO
-        return {torch::zeros({1}), torch::zeros({1})};
-}
