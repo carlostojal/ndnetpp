208aadcc1e91868fe92ebb2faaf84039ba3dc047
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-08-16 19:36:24 +0100

    implement metric to voxel space mapping

--- nd_utils/voxelization.py
+++ nd_utils/voxelization.py
@@ -23,7 +23,7 @@ def find_point_cloud_limits(point_cloud: torch.Tensor) -> Tuple[torch.Tensor, to
 
     return min_coords, max_coords, dimensions
 
-def calculate_voxel_size(dimensions: torch.Tensor, n_desired_voxels: int) -> torch.Tensor:
+def calculate_voxel_size(dimensions: torch.Tensor, n_desired_voxels: int) -> Tuple[float, torch.Tensor]:
     """
     Calculate the voxel size considering the point cloud characteristics and desired number of voxels
     (don't confuse with normal distributions. voxels without samples don't count as normal distributiondon't confuse with normal distributions. voxels without samples don't count as normal distributionss)
@@ -44,3 +44,27 @@ def calculate_voxel_size(dimensions: torch.Tensor, n_desired_voxels: int) -> tor
     n_voxels = torch.ceil(dimensions / voxel_size)
 
     return voxel_size, n_voxels
+
+def metric_to_voxel_space(points: torch.Tensor, voxel_size: float, n_voxels: torch.Tensor,
+                          min_coords: torch.Tensor) -> torch.Tensor:
+    """
+    Map a point in metric space to voxel space.
+
+    Args:
+        points (torch.Tensor): Coordinates of the points (n_points, 3)
+        voxel_size (float): Voxel size
+        n_voxels (torch.Tensor): Number of voxels in each dimension (3)
+        min_coords (torch.Tensor): Minimum coordinate values in metric space (3)
+
+    Returns:
+        voxel_idx (torch.Tensor): Voxel indices in each dimension (3)
+    """
+
+    voxel_idx = torch.floor((points - min_coords) / voxel_size)
+
+    # check out of bounds indices
+    out_of_bounds = (voxel_idx < 0) | (voxel_idx >= n_voxels)
+    if torch.any(out_of_bounds):
+        raise ValueError("Point coordinates out of point cloud bounds.")
+    
+    return voxel_idx
