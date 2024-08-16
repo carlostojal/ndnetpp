f87a745cb07a804162c9f3edecb1314b700afa06
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-08-16 17:49:04 +0100

    make the structure of the voxelization function

--- models/ndnetpp/nd.py
+++ models/ndnetpp/nd.py
@@ -1,5 +1,36 @@
+import torch
 from torch import nn
 import nd_utils
+
+class VoxelizerFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input: torch.Tensor, num_desired_dist: int, num_desired_dist_thres: float = 0.2):
+        """
+        Voxelize the input point cloud.
+
+        Args:
+            input (torch.Tensor): Input point cloud of shape (N, 3).
+            num_desired_dist (int): Number of desired normal distributions.
+            num_desired_dist_thres (float): Threshold for the number of desired normal distributions.
+
+        Returns:
+            torch.Tensor: Normal distribution means (N1, 3).
+            torch.Tensor: Normal distribution covariances (N1, 3, 3).
+        """
+        raise NotImplementedError("VoxelizerFunciton.forward is not implemented.")
+
+    @staticmethod
+    def backward(ctx, dists_grad: torch.Tensor):
+        """
+        Voxelization backward pass.
+
+        Args:
+            dists_grad (torch.Tensor): gradients of the distributions (voxels) losses (N1).
+
+        Returns:
+            torch.Tensor: gradients propagated to the points corresponding to each voxel (N).
+        """
+        raise NotImplementedError("VoxelizerFunction.backward is not implemented.")
 
 class Voxelizer(nn.Module):
     """
