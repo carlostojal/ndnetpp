0e615415a217ab5f28c7515bb4ec2965d176316a
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-08-17 18:36:27 +0100

    update the limits lookup to handle the batch dimension

--- nd_utils/voxelization.py
+++ nd_utils/voxelization.py
@@ -31,17 +31,17 @@ def find_point_cloud_limits(point_cloud: torch.Tensor) -> Tuple[torch.Tensor, to
     Find the limits of the point cloud coordinates in each axis.
 
     Args:
-        point_cloud (torch.Tensor): Point cloud data (n_points, 3)
+        point_cloud (torch.Tensor): Point cloud data (batch_size, n_points, 3)
 
     Returns:
-        torch.Tensor: minimum limits (3)
-        torch.Tensor: maximum limits (3)
-        torch.Tensor: dimension in each axis(3)
+        torch.Tensor: minimum limits (batch_size, 3)
+        torch.Tensor: maximum limits (batch_size, 3)
+        torch.Tensor: dimension in each axis (batch_size, 3)
     """
 
     # find the limits
-    max_coords = torch.max(point_cloud, dim=0).values
-    min_coords = torch.min(point_cloud, dim=0).values
+    max_coords = torch.max(point_cloud, dim=1).values
+    min_coords = torch.min(point_cloud, dim=1).values
 
     # calculate the dimensions in each axis (max - min)
     dimensions = max_coords - min_coords
