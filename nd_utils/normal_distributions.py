3dca9228800423b041affe50b587f707ad457f69
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-08-20 18:04:26 +0100

    do some fixes on normal distribution calculations to handle batches. outline future work

--- nd_utils/normal_distributions.py
+++ nd_utils/normal_distributions.py
@@ -25,48 +25,52 @@ SOFTWARE.
 
 import torch
 from typing import Tuple
-import nd_utils.voxelization
+import nd_utils
 
 def estimate_normal_distributions(points: torch.Tensor, n_desired_dists: int,
-                                  n_desired_dists_thres: float = 0.2) -> Tuple[torch.Tensor, torch.Tensor]:
+                                  n_desired_dists_thres: float = 0.2) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """
     Estimate normal distributions from the point coordinates.
 
     Args:
         points (torch.Tensor): Point coordinates (batch_size, n_points, 3)
         n_desired_dists (int): Desired number of normal distributions
+        n_desired_dists_thres (float): Threshold of acceptable number of normal distributions
+
 
     Returns:
-        means: Mean vectors (batch_size, voxels_x, voxels_y, voxels_z, 3)
-        covs: Covariance matrices (batch_size, voxels_x, voxels_y, voxels_z, 3, 3)
+        dists (torch.Tensor): Concatenated mean vectors and covariance matrices (batch_size, voxels_x, voxels_y, voxels_z, 12)
+        sample_counts (torch.Tensor): Sample counts for each voxel (batch_size, voxels_x, voxels_y, voxels_z)
     """
+
+    # TODO: fix fuckups in batch dimensions and other things
 
     # find the point cloud limits and ranges
     min_coords, max_coords, dimensions = nd_utils.voxelization.find_point_cloud_limits(points)
 
     # calculate the voxel size
-    voxel_size, n_voxels = nd_utils.voxelization.calculate_voxel_size(dimensions, n_desired_dists*(1.0+n_desired_dists_thres))
+    voxel_size, n_voxels = nd_utils.voxelization.calculate_voxel_size(dimensions, int(n_desired_dists*(1.0+n_desired_dists_thres)))
 
-    # create a tensor of means with shape (voxels_x, voxels_y, voxels_z, 3)
-    means = torch.zeros(torch.cat(n_voxels, torch.Tensor([3])).int().tolist())
+    # create a tensor of means with shape (batch_size, voxels_x, voxels_y, voxels_z, 3)
+    means = torch.zeros(torch.cat((torch.tensor([points.shape[0]]), n_voxels, torch.tensor([[3]])), dim=1).int().tolist()[0])
 
-    # create a tensor of covariances with shape (voxels_x, voxels_y, voxels_z, 3, 3)
-    covs = torch.zeros(torch.cat(n_voxels, torch.Tensor([3, 3])).int().tolist())
+    # create a tensor of covariances with shape (batch_size, voxels_x, voxels_y, voxels_z, 3, 3)
+    covs = torch.zeros(torch.cat((torch.tensor([points.shape[0]]), n_voxels, torch.tensor([[3, 3]])), dim=1).int().tolist())
 
-    # create a tensor of sample counts with shape (voxels_x, voxels_y, voxels_z)
-    sample_counts = torch.zeros(n_voxels.int().tolist()).int()
+    # create a tensor of sample counts with shape (batch_size, voxels_x, voxels_y, voxels_z)
+    sample_counts = torch.zeros(n_voxels.tolist()).int()
 
-    # get the voxel indices for each point with shape (n_points, 3)
+    # get the voxel indices for each point with shape (batch_size, n_points, 3)
     voxel_idxs = nd_utils.voxelization.metric_to_voxel_space(points, voxel_size, n_voxels, min_coords)
 
     # increment the sample counts for each voxel
-    sample_counts = sample_counts.scatter_add(0, voxel_idxs, torch.ones(voxel_idxs.size(0)).int())
+    sample_counts = sample_counts.scatter_add_(1, voxel_idxs, torch.ones(voxel_idxs.size(1)).int())
 
     # calculate the means
-    means = means.scatter_add(0, voxel_idxs.unsqueeze(1).expand(-1, 3, -1), points.unsqueeze(2).expand(-1, -1, 3))
+    means = means.scatter_add_(1, voxel_idxs.unsqueeze(1).expand(-1, 3, -1), points.unsqueeze(2).expand(-1, -1, 3))
 
     # calculate the covariances
-    covs = covs.scatter_add(0, voxel_idxs.unsqueeze(1).unsqueeze(1).expand(-1, 3, 3, -1),
+    covs = covs.scatter_add_(1, voxel_idxs.unsqueeze(1).unsqueeze(1).expand(-1, 3, 3, -1),
                             (points.unsqueeze(2).expand(-1, -1, 3) - means[voxel_idxs].unsqueeze(3)).unsqueeze(3))
     
     # divide the means by the sample counts
@@ -75,4 +79,23 @@ def estimate_normal_distributions(points: torch.Tensor, n_desired_dists: int,
     # divide the covariances by the sample counts
     covs = covs / sample_counts.unsqueeze(3).unsqueeze(4)
 
-    return means, covs
+    # TODO: concatenate the means and covariances
+
+    return means, covs, sample_counts
+
+def prune_normal_distributions(means: torch.Tensor, covs: torch.Tensor, valid_dists: torch.Tensor, n_desired_dists: int) -> torch.Tensor:
+    """
+    Prune the extra normal distributions based on the Kullback-Leibler divergences.
+
+    Args:
+        means (torch.Tensor): Tensor of normal distribution means (batch_size, voxels_x, voxels_y, voxels_z, 3)
+        covs (torch.Tensor): Tensor of normal distribution covariances (batch_size, voxels_x, voxels_y, voxels_z, 3, 3)
+        n_desired_dists (int): Number of desired normal distributions after the pruning proces.
+
+    Returns:
+        valid_dists (torch.Tensor): Tensor of booleans denoting either a normal distribution is valid or not.
+    """
+
+    # TODO
+    raise NotImplementedError("Kullback-Leibler normal distribution pruning not implemented.")
+
