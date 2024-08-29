d2c35ed7ad850beb7fa58df060713d62a2729774
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-08-29 16:52:09 +0100

    implement a new function to estimate the grid from the voxel size

--- nd_utils/normal_distributions.py
+++ nd_utils/normal_distributions.py
@@ -26,14 +26,15 @@ SOFTWARE.
 import torch
 from typing import Tuple
 import nd_utils
+import nd_utils.voxelization
 
-def estimate_normal_distributions(points: torch.Tensor, n_desired_dists: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+def estimate_normal_distributions_with_numel(points: torch.Tensor, n_elements: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """
-    Estimate normal distributions from the point coordinates.
+    Estimate normal distributions within a grid with a given number of elements from the point coordinates.
 
     Args:
         points (torch.Tensor): Point coordinates (batch_size, n_points, 3)
-        n_desired_dists (int): Desired number of normal distributions
+        n_desired_dists (int): Desired number grid elements.
 
 
     Returns:
@@ -47,8 +48,57 @@ def estimate_normal_distributions(points: torch.Tensor, n_desired_dists: int) ->
     min_coords, _, dimensions = nd_utils.voxelization.find_point_cloud_limits(points)
 
     # calculate the batch-wise voxel size and number of voxels
-    voxel_size, n_voxels = nd_utils.voxelization.calculate_voxel_size(dimensions, int(n_desired_dists))
+    voxel_size, n_voxels = nd_utils.voxelization.calculate_voxel_size(dimensions, int(n_elements))
 
+    # estimate the normal distribution grid
+    dists, sample_counts = estimate_grid(points, min_coords, n_voxels, voxel_size)
+
+    return dists, sample_counts, min_coords, voxel_size
+
+
+def estimate_normal_distributions_with_size(points: torch.Tensor, voxel_size: float) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Estimate normal distributions with a given voxel size from the point coordinates.
+
+    Args:
+        points (torch.Tensor): Point coordinates (batch_size, n_points, 3)
+        n_desired_dists (int): Desired number of normal distributions
+
+    Returns:
+        dists (torch.Tensor): Concatenated mean vectors and covariance matrices (batch_size, voxels_x, voxels_y, voxels_z, 12)
+        sample_counts (torch.Tensor): Sample counts for each voxel (batch_size, voxels_x, voxels_y, voxels_z)
+        min_coords (torch.Tensor): Point cloud minimum coordinates in each axis
+        n_voxels (torch.Tensor): Number of voxels in each dimension
+    """
+
+    # find point cloud limits and dimensions
+    min_coords, _, dimensions = nd_utils.voxelization.find_point_cloud_limits(points)
+
+    # calculate the number of voxels in each dimension
+    n_voxels = nd_utils.voxelization.calculate_num_voxels(dimensions, voxel_size)
+
+    # estimate the normal distribution grid
+    dists, sample_counts = estimate_grid(points, min_coords, n_voxels, voxel_size)
+
+    return dists, sample_counts, min_coords, n_voxels
+
+
+def estimate_grid(points: torch.Tensor, min_coords: torch.Tensor, n_voxels: torch.Tensor, voxel_size: float) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Estimate the normal distributions grid from the points and all grid information.
+
+    Args:
+        points (torch.Tensor): Point coordinates (batch_size, n_points, 3)
+        min_coords (torch.Tensor): Minimum point coordinates of the point cloud in each axis
+        n_voxels (torch.Tensor): Number of voxels in each dimension
+        voxel_size (float): Size of each voxel edge
+    
+    Returns:
+        dists (torch.Tensor): Concatenated mean vectors and flattened covariance matrices (batch_size, voxels_x, voxels_y, voxels_x, 12)
+        sample_counts (torch.Tensor): Sample counts for each voxel (batch_size, voxels_x, voxels_y, voxels_z)
+    """
+
+    # build the grid dimension with the batch dimension
     grid_dim = torch.cat((torch.tensor([points.shape[0]]), n_voxels.cpu())).int()
 
     # create a tensor of means with shape (batch_size, voxels_x, voxels_y, voxels_z, 3)
@@ -92,7 +142,7 @@ def estimate_normal_distributions(points: torch.Tensor, n_desired_dists: int) ->
     # concatenate the means and covariances along the mean/flattened covariance dimension (last dimension)
     dists = torch.cat((means, covs), dim=-1)
 
-    return dists, sample_counts, min_coords, voxel_size
+    return dists, sample_counts
 
 def prune_normal_distributions(means: torch.Tensor, covs: torch.Tensor, valid_dists: torch.Tensor, n_desired_dists: int) -> torch.Tensor:
     """
