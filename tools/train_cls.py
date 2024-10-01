87fc8409acf4b7e780fb5cb022885ab3541a96d2
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-10-01 19:27:22 +0100

    parse command line arguments and load configuration file

--- tools/train_cls.py
+++ tools/train_cls.py
@@ -22,12 +22,45 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 import torch
-import argparse
+from argparse import ArgumentParser
+import yaml
 import sys
+import os
 sys.path.append(".")
 from models.ndnetpp.ndnetpp_cls import NDNetppClassifier
 
 if __name__ == '__main__':
+
+    # parse command line arguments
+    parser = ArgumentParser()
+    parser.add_argument("--config", type=str, default="configs/ndnetpp_cls.yaml", 
+                        help="Path to the YAML network configuration file")
+    parser.add_argument("--data_path", type=str,
+                        help="Path to the ModelNet40 dataset", required=True)
+    args = parser.parse_args()
+
+    # check the configuration file for existance and parse
+    if not os.path.exists(args.config):
+        raise FileNotFoundError(f"The configuration file \"{args.config}\" does not seem to exist.")
+    # open the file descriptor
+    try:
+        f = open(args.config)
+    except Exception as e:
+        raise RuntimeError(f"Error opening configuration file: {repr(e)}")
+    # parse the configuration file
+    try:
+        config = yaml.safe_load(f)
+    except Exception as e:
+        raise RuntimeError(f"Error loading the network configuration file: {repr(e)}")
+    # close the file descriptor
+    try:
+        f.close()
+    except Exception as e:
+        raise RuntimeError(f"Error closing configuration file: {repr(e)}")
+
+    print(config)
+
+
 
     # TODO: parse command line arguments
     # TODO: read the configuration file
@@ -38,4 +71,7 @@ if __name__ == '__main__':
     # TODO: save pth files with the weights
 
     raise NotImplementedError("Classifier training loop not implemented")
+
+    # exit with code 0
+    exit(0)
 
