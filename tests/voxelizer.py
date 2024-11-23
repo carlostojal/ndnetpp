2cfab3c898f5c2829ce5051debc810099b66849d
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-11-23 20:04:03 +0000

    create voxel size and count calculation tests

--- tests/voxelizer.py
+++ tests/voxelizer.py
@@ -1,10 +1,10 @@
 import unittest
 import torch
-from nd_utils.voxelization import find_point_cloud_limits
+from nd_utils.voxelization import find_point_cloud_limits, calculate_voxel_size
 
 class TestVoxelizer(unittest.TestCase):
 
-    def test_find_limits(self):
+    def test_find_limits(self) -> None:
 
         pcd = torch.tensor([[
             [-1, -1, -1],
@@ -16,9 +16,21 @@ class TestVoxelizer(unittest.TestCase):
             [1, 1, -1],
             [1, 1, 1]
         ]])
-
+    
+        print("Finding point cloud limits...")
         min_coords, max_coors, dimensions = find_point_cloud_limits(pcd)
 
+        print("Testing point cloud limits...")
         self.assertTrue(torch.equal(min_coords, torch.tensor([-1, -1, -1])))
         self.assertTrue(torch.equal(max_coors, torch.tensor([1, 1, 1])))
         self.assertTrue(torch.equal(dimensions, torch.tensor([2, 2, 2])))
+
+    def test_calculate_voxel_size(self) -> None:
+
+        print("Calculating voxel size and count...")
+        voxel_size, n_voxels = calculate_voxel_size(torch.tensor([10, 10, 2]), 200)
+
+        print("Testing voxel size and count...")
+        self.assertAlmostEqual(voxel_size, 1.0)
+        self.assertTrue(torch.equal(torch.tensor([10, 10, 2]), n_voxels))
+
