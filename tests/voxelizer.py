4900e713e6714b69e5a6a4e04f609b8868382e25
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-11-25 23:30:01 +0000

    test voxel to metric space conversion

--- tests/voxelizer.py
+++ tests/voxelizer.py
@@ -53,5 +53,22 @@ class TestVoxelizer(unittest.TestCase):
         print("Testing voxel indices...")
         self.assertTrue(torch.equal(pcd, voxel_idx))
 
+    def test_convert_voxel_to_metric_space(self) -> None:
 
+        voxel_idx_l: List[torch.Tensor] = []
+
+        for x in range(2):
+            for y in range(3):
+                for z in range(3):
+                    voxel_idx_l.append(torch.tensor([x, y, z]))
+
+        voxel_idx = torch.stack(voxel_idx_l, dim=0).unsqueeze(0)
+
+        print("Converting voxel indices to metric space...")
+        min_coords, _, _ = find_point_cloud_limits(voxel_idx)
+        min_coords = min_coords.float()
+        pcd = voxel_to_metric_space(voxel_idx, 1, min_coords)
+
+        print("Testing voxel indices...")
+        self.assertTrue(torch.equal(pcd, voxel_idx))
 
